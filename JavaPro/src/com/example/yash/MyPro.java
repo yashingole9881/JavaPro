326e3ddb4908529ec63da6535cd73c91b3d37305
yashingole9881 <65347812+yashingole9881@users.noreply.github.com>
2020-09-29 16:42:11 +0530

    Adding new student object

--- JavaPro/src/com/example/yash/MyPro.java
+++ JavaPro/src/com/example/yash/MyPro.java
@@ -5,8 +5,10 @@ public class MyPro {
 	public static void main(String[] args) {
 		
 		Student s1=new Student("java",123); 
+		Student s2=new Student();
+		
 		System.out.println(s1);
-
+		System.out.println(s2);
 	}
 
 }
